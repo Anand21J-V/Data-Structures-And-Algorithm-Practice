0952e2ce9b691300598c77bceeaf7af3d4dba78b
Anand21J-V <anandvishwakarma21j.com>
2025-05-21 01:41:27 +0530

    Recursive Binary Search Tree is Done from Scratch

--- Trees/Binary_Search_Tree.py
+++ Trees/Binary_Search_Tree.py
@@ -60,14 +60,44 @@ class Binary_search_tree():
     def recursive_insert(self, current_node, value):
         if current_node == None:
             return Node(value)
+        
         if value < current_node.value:
             current_node.left = self.recursive_insert(current_node.left, value)
         if value > current_node.value:
             current_node.right = self.recursive_insert(current_node.right, value)
-        return current_node.value        
 
-    def r_contains(self, value): 
-        return self.recursive_insert(self.root, value)  
+        return current_node.value        
+    
+
+    def min_value(self, current_node):
+        while (current_node.left is not None):
+            current_node = current_node.left
+        return current_node.value 
+
+    def __delete_node(self, current_node, value):
+        if current_node == None: 
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value: 
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+    
+        
+
+    def delete_node(self, value):
+        self.__delete_node(self.root, value) 
         
 
 My_tree = Binary_search_tree()
@@ -79,6 +109,7 @@ My_tree.insert(10)
 My_tree.insert(6)
 My_tree.insert(13)
 My_tree.insert(15)
+
 
 
 print(My_tree.r_contains(20))

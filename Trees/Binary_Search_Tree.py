c8f4b0f1524b7612bf4f2235a8e07ffe4fc06073
Anand21J-V <anandvishwakarma21j.com>
2025-05-20 16:59:02 +0530

    Bst

--- Trees/Binary_Search_Tree.py
+++ Trees/Binary_Search_Tree.py
@@ -47,14 +47,40 @@ class Binary_search_tree():
         return False
 
 
+    def recursive_contains(self,current_node, value):
+        if current_node == None:
+            return False
+        if value == current_node.value:
+            return True 
+        if value < current_node.value:
+            return self.recursive_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.recursive_contains(current_node.right, value)
+        
+    def recursive_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.recursive_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.recursive_insert(current_node.right, value)
+        return current_node.value        
+
+    def r_contains(self, value): 
+        return self.recursive_insert(self.root, value)  
+        
 
 My_tree = Binary_search_tree()
 My_tree.insert(2)
 My_tree.insert(1)
 My_tree.insert(3)
-My_tree.contains(1)
+My_tree.insert(12)
+My_tree.insert(10)
+My_tree.insert(6)
+My_tree.insert(13)
+My_tree.insert(15)
 
 
-print(My_tree.contains(1))
+print(My_tree.r_contains(20))
 
 
